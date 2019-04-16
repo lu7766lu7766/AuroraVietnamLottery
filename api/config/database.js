8a048e1ad88ac273d7e44abfc33ba3c14ff44bf1
Jac <lu7766lu7766@gmail.com>
2019-04-17 00:37:32 +0800

    debug判斷方式修改

--- api/config/database.js
+++ api/config/database.js
@@ -55,7 +55,7 @@ module.exports = {
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
-      debug: Env.get('DB_DEBUG', false)
+      debug: Env.get('DB_DEBUG', 'false') === 'true'
       // timezone: 'utc'
     }
   },
